d16e50e6c1391557fdd104462182943437f6fa86
razaquegoraya007 <razaqueg007@gmail.com>
2024-02-12 16:55:11 +0500

    js

--- week4/classcode/index.js
+++ week4/classcode/index.js
@@ -1,1 +1,19 @@
-console.log("Hello this is me Javascript")
+// console.log("Hello this is me Javascript")
+let myobject = {
+    name:"Abdul",
+    cms:'021-21-0081',
+    address:{studname:"Ahsan",city:"sukkur"}
+};
+// console.log(myobject['name'])
+// console.log(Object.keys(myobject))
+let keyss = Object.keys(myobject)
+// console.log(myobject[keyss[1]])
+
+for (let key in myobject){
+    console.log(myobject[key])
+    if(Object.keys(myobject[key].length>0)){
+        for(let key2 in myobject[key]){
+            console.log(myobject[key][key2])
+        }
+    }
+}
